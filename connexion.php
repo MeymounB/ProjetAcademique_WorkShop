010c2f1258961eb8978e73485a34e780f6f827a4
ReadyCash2222 <77726988+ReadyCash2222@users.noreply.github.com>
2021-09-07 11:53:59 +0200

    Création page connexion

--- connexion.php
+++ connexion.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html lang="fr">
+
+<?php
+try {
+	// Sous WAMP
+$bdd = new PDO('mysql:host=localhost;dbname=workshop;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+	die('Erreur : ' . $e->getMessage());
+}
+?>
+
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>WorkShop</title>
 </head>
-<body>
+	<body>
 	
-</body>
+	</body>
 </html>
