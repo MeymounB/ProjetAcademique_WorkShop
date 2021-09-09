e8fe328d2c1468db6a6fffc083d309f989ab7add
ReadyCash2222 <77726988+ReadyCash2222@users.noreply.github.com>
2021-09-09 22:23:30 +0200

    profil

--- espace_membre.php
+++ espace_membre.php
@@ -4,9 +4,15 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Profil</title>
+	<link rel="stylesheet" href="../WorkShop//css/style.css">
+	<link rel="stylesheet" href="">
 </head>
 <body>
-		<p>hi</p>
+	<div>
+		<h1>Profile</h1>
+	</div>
+
+
 </body>
 </html>
